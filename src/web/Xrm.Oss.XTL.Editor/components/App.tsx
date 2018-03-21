a98a212351077713e635022acb2a65bf47ff5616
Florian Krönert <Florian.Kroenert@silflo.de>
2018-03-22 00:30:30 +0100

    Extended editor

--- src/web/Xrm.Oss.XTL.Editor/components/App.tsx
+++ src/web/Xrm.Oss.XTL.Editor/components/App.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 import WebApiClient from "xrm-webapi-client";
-import { Well, ButtonToolbar, ButtonGroup, Button, DropdownButton, MenuItem, Modal } from "react-bootstrap";
+import { Well, ButtonToolbar, ButtonGroup, Button, DropdownButton, MenuItem, Modal, FormGroup, ControlLabel, FormControl } from "react-bootstrap";
 import * as Parser from "html-react-parser";
 
 interface WYSIWYGEditorState {
   inputTemplate: string;
+  executionCriteria: string;
   selectedEntityLogicalName: string;
   selectedEntityId: string;
   selectedEntityName: string;
@@ -15,6 +16,7 @@ interface WYSIWYGEditorState {
   entities: Array<any>;
   selectedTypeCode: number;
   requestPending: boolean;
+  copyTemplate: boolean;
 }
 
 export default class WYSIWYGEditor extends React.PureComponent<any, WYSIWYGEditorState> {
@@ -28,22 +30,27 @@ export default class WYSIWYGEditor extends React.PureComponent<any, WYSIWYGEdito
           selectedEntityName: "",
           selectedEntityId: "",
           inputTemplate: "",
+          executionCriteria: "",
           resultText: "",
           traceLog: "",
           error: "",
           success: true,
           entities: [],
           selectedTypeCode: 0,
-          requestPending: false
+          requestPending: false,
+          copyTemplate: false
         };
 
         // Webpack should import WebApiClient from global itself, but somehow it doesn't
         this.WebApiClient = (window as any).WebApiClient;
 
         this.inputChanged = this.inputChanged.bind(this);
+        this.criteriaChanged = this.criteriaChanged.bind(this);
         this.preview = this.preview.bind(this);
         this.selectTarget = this.selectTarget.bind(this);
         this.setTypeCode = this.setTypeCode.bind(this);
+        this.copy = this.copy.bind(this);
+        this.closeCopyDialog = this.closeCopyDialog.bind(this);
     }
 
     componentDidMount() {
@@ -72,7 +79,8 @@ export default class WYSIWYGEditor extends React.PureComponent<any, WYSIWYGEdito
                         Id: this.state.selectedEntityId,
                         LogicalName: this.state.selectedEntityLogicalName
                     },
-                    template: this.state.inputTemplate
+                    template: this.state.inputTemplate,
+                    executionCriteria: this.state.executionCriteria
                 })
             }
         }))
@@ -90,7 +98,11 @@ export default class WYSIWYGEditor extends React.PureComponent<any, WYSIWYGEdito
     selectTarget(e: any) {
         const url = this.WebApiClient.GetApiUrl().replace("/api/data/v8.0/", "") + `/_controls/lookup/lookupinfo.aspx?AllowFilterOff=1&DisableQuickFind=0&DisableViewPicker=0&LookupStyle=single&ShowNewButton=0&ShowPropButton=0&browse=false&objecttypes=${this.state.selectedTypeCode}`;
         const Xrm: any = (window as any).Xrm;
-        Xrm.Internal.openDialog(url , {width: 300, height: 500}, undefined, undefined, (result: any) => {
+        const options = new Xrm.DialogOptions();
+        options.width = 500;
+        options.height = 800;
+
+        Xrm.Internal.openDialog(url , options, undefined, undefined, (result: any) => {
             const reference = result.items[0];
 
             this.setState({
@@ -107,9 +119,27 @@ export default class WYSIWYGEditor extends React.PureComponent<any, WYSIWYGEdito
       });
     }
 
+    criteriaChanged(e: any) {
+      this.setState({
+        executionCriteria: e.target.value
+      });
+    }
+
     setTypeCode(eventKey: any) {
         this.setState({
             selectedTypeCode: parseInt(eventKey)
+        });
+    }
+
+    copy() {
+        this.setState({
+            copyTemplate: true
+        });
+    }
+
+    closeCopyDialog() {
+        this.setState({
+            copyTemplate: false
         });
     }
 
@@ -124,10 +154,21 @@ export default class WYSIWYGEditor extends React.PureComponent<any, WYSIWYGEdito
 
             <Modal.Body>Your template is being processed on the server...</Modal.Body>
           </Modal.Dialog>}
+          {this.state.copyTemplate &&
+            <Modal.Dialog>
+            <Modal.Header>
+              <Modal.Title>Copy Json below</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>{JSON.stringify({template: this.state.inputTemplate, executionCriteria: this.state.executionCriteria})}</Modal.Body>
+            <Modal.Footer>
+                <Button bsStyle="default" onClick={ this.closeCopyDialog }>Close</Button>
+            </Modal.Footer>
+          </Modal.Dialog>}
           {this.state.selectedEntityId && <a>Entity: {this.state.selectedEntityLogicalName}, Id: {this.state.selectedEntityId}, Name: {this.state.selectedEntityName}</a>}
           {!this.state.success && <a>"Error: {this.state.error}</a>}
           <div>
-            <ButtonToolbar>
+            <ButtonToolbar style={{"padding-bottom": "10px"}}>
               <ButtonGroup>
                 <DropdownButton
                     bsStyle="default"
@@ -137,12 +178,24 @@ export default class WYSIWYGEditor extends React.PureComponent<any, WYSIWYGEdito
                       { this.state.entities.map( value => <MenuItem onSelect={this.setTypeCode} eventKey={value.ObjectTypeCode}>{value.SchemaName}</MenuItem> ) }
                 </DropdownButton>
                 <Button bsStyle="default" disabled={this.state.selectedTypeCode === 0} onClick={ this.selectTarget }>Select Target</Button>
-                <Button bsStyle="default" onClick={ this.preview }>Preview</Button>
+                <Button bsStyle="default" disabled={!this.state.selectedEntityId || !this.state.selectedTypeCode} onClick={ this.preview }>Preview</Button>
+                <Button bsStyle="default" onClick={ this.copy }>Copy Current Template</Button>
               </ButtonGroup>
             </ButtonToolbar>
-              <textarea className="col-xs-6" style={ { "height": "100vh" } } onChange={ this.inputChanged } />
-              <div className="col-xs-6" style={ { "height": "50vh" } }>{Parser(this.state.resultText)}</div>
-              <textarea className="col-xs-6" style={ { "height": "50vh" } } value={ this.state.traceLog } disabled />
+              <FormGroup className="col-xs-6" controlId="input">
+                <ControlLabel>Execution Criteria</ControlLabel>
+                <FormControl style={ { "height": "25vh" } } onChange={ this.criteriaChanged } componentClass="textarea" placeholder="Leave empty for executing unconditionally" />
+                <ControlLabel style={{"padding-top": "10px"}}>Template</ControlLabel>
+                <FormControl style={ { "height": "75vh" } } onChange={ this.inputChanged } componentClass="textarea" placeholder="Enter template" />
+              </FormGroup>
+              <div className="col-xs-6">
+                <ControlLabel>Result</ControlLabel>
+                <div style={ { "height": "50vh", "border": "1px solid lightgray" } }>{Parser(this.state.resultText)}</div>
+                <FormGroup controlId="output">
+                  <ControlLabel style={{"padding-top": "10px"}}>Interpreter Trace</ControlLabel>
+                  <FormControl style={ { "height": "50vh" } } onChange={ this.criteriaChanged } componentClass="textarea" value={ this.state.traceLog } disabled />
+                </FormGroup>
+              </div>
           </div>
         </div>
         );
