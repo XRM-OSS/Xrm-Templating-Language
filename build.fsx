4ce15e55c0d2518ba153990165afffacdf47ca64
Florian Krönert <DigitalFlow@users.noreply.github.com>
2020-05-08 11:08:43 +0200

    Version bump

--- build.fsx
+++ build.fsx
@@ -35,7 +35,7 @@ let sha = Git.Information.getCurrentHash()
 // version info
 let major = "3"
 let minor = "6"
-let patch = "1"
+let patch = "3"
 
 // Follow SemVer scheme: http://semver.org/
 let asmVersion = major + "." + minor + "." + patch
