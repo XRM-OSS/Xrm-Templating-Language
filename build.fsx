abaef9aa5d8148f29653519fdc770f632895bc6c
Florian Krönert <Florian.Kroenert@orbis.de>
2020-10-20 12:32:26 +0200

    Version bump

--- build.fsx
+++ build.fsx
@@ -34,8 +34,8 @@ let sha = Git.Information.getCurrentHash()
 
 // version info
 let major = "3"
-let minor = "7"
-let patch = "4"
+let minor = "8"
+let patch = "0"
 
 // Follow SemVer scheme: http://semver.org/
 let asmVersion = major + "." + minor + "." + patch
