2a96fefae91b98b63804bbad0f21f6115b1215b7
Florian Kroenert <Florian.Kroenert@mailbox.org>
2023-02-17 07:26:23 +0100

    Added GptPrompt to function handlers

--- src/lib/Xrm.Oss.XTL.Interpreter/FunctionHandlers.cs
+++ src/lib/Xrm.Oss.XTL.Interpreter/FunctionHandlers.cs
@@ -5,7 +5,7 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Net.Http;
+using System.Net;
 using System.Text.RegularExpressions;
 using Microsoft.Crm.Sdk.Messages;
 using Microsoft.Xrm.Sdk;
